a5aca225829d3187b089d894cc9fbef39ce6659d
Aegis1979 <167387811+Aegis1979@users.noreply.github.com>
2024-04-26 16:44:11 +0930

    Create sonic.py

---
+++ sonic.py
@@ -0,0 +1,34 @@
+#Raspberry Pi 4.0 code for interfacing with ultrasonic sensor
+
+import Rpi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+TRI_PIN=11
+ECHO_PIN=12
+
+GPIO.setup(TRIG_PIN, GPIO.OUT)
+GPIO.setup(ECHO_PIN, GPIO.IN)
+GPIO.output(TRIG_PIN, GPIO.LOW)
+
+time.sleep(2)
+
+GPIO.output(TRIG_PIN,GPIO.HIGH)
+
+time.sleep(0.00001)
+GPIO.output(TRIG_PIN, GPIO.LOW)
+
+while GPIO.input(ECHO_PIN)==0:
+  pulse_send=time.time()
+
+while GPIO.input(ECHO_PIN)==1:
+  pulse_received=time.time()
+
+pulse_duration= pulse_received- pulse_send
+
+pulse_duration= round(pulse_duration/2)
+
+distance=34000*pulse_duration
+
+print('Distance to object:',distance)
+GPIO.cleanup()
